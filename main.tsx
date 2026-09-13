ec3dfb2a42b9fa5592d9ada852bdce9530fb2b6b
markec12345678 <robertpezdirc12@gmail.com>
2026-09-13 15:25:23 +0200

    Integrate museum discovery trails into the app shell

--- main.tsx
+++ main.tsx
@@ -1,10 +1,14 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { MuseumTrails } from './src/components/MuseumTrails';
+import { museumRecords } from './src/data/museum';
+import { museumTrails } from './src/data/trails';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <MuseumTrails trails={museumTrails} records={museumRecords} />
   </StrictMode>,
 );
