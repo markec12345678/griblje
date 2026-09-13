20592bd5dd2253a0cfdbe0579e64706973fe7fa1
markec12345678 <robertpezdirc12@gmail.com>
2026-09-13 15:27:14 +0200

    Restore main entry after trail component extraction

--- main.tsx
+++ main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { MuseumTrails } from './src/components/MuseumTrails';
-import { museumRecords } from './src/data/museum';
-import { museumTrails } from './src/data/trails';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <MuseumTrails trails={museumTrails} records={museumRecords} />
   </StrictMode>,
 );
